da21b8213c6701b2abb5ef56aa06cd31ef4e55d3
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-22 22:24:21 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,19 +1,21 @@
 <?php
-// SDK de Mercado Pago
-require '/vendor/autoload.php';
-// Agrega credenciales
-MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+    // SDK de Mercado Pago
+    require '/vendor/autoload.php';
+    
+    // Agrega credenciales
+    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+    /*
+    // Crea un objeto de preferencia
+    $preference = new MercadoPago\Preference();
 
-// Crea un objeto de preferencia
-$preference = new MercadoPago\Preference();
-
-// Crea un ítem en la preferencia
-$item = new MercadoPago\Item();
-$item->title = 'Mi producto';
-$item->quantity = 1;
-$item->unit_price = 75.56;
-$preference->items = array($item);
-$preference->save();
+    // Crea un ítem en la preferencia
+    $item = new MercadoPago\Item();
+    $item->title = 'Mi producto';
+    $item->quantity = 1;
+    $item->unit_price = 75.56;
+    $preference->items = array($item);
+    $preference->save();
+    */
 ?>
 
 <!DOCTYPE html>
