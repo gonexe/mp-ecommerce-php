d2758d356892cf09f3345a3e2a555f0e81d74ea8
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-22 22:26:32 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,9 +1,9 @@
 <?php
     // SDK de Mercado Pago
-    require '/vendor/autoload.php';
+    #require '/vendor/autoload.php';
     
     // Agrega credenciales
-    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+    #MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     /*
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();
