e272a0946ed72580d5cae51c07119cadf84eff3c
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-24 00:26:07 -0300

    commit

--- detail2.php
+++ detail2.php
@@ -68,7 +68,7 @@
 
             <script
                 src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-                data-preference-id="<?php echo $preference->id; ?>">
+                data-preference-id="<?php echo $preference -> id; ?>">
             </script>
 
         </form>
