7d9041dd56e7ddaaaacd4fa88822332a5a7ea314
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-24 02:20:33 -0300

    1

--- pending.php
+++ pending.php
@@ -1,12 +1,12 @@
 <?php
 
-    print_r('Visualización de resultado PENDIENTE ($_GET)');
-    print_r('<br>');
+    print_r('<h2>Visualización de resultado PENDIENTE ($_GET):</h2>');
+    print_r("<br>");
     foreach ($_GET as $key => $value) {
-        print_r("Clave: $key - Valor: $value");
-        print_r('<br>');
+        print_r("$key = $value");
+        print_r("<br>");
     }
-    print_r('<br><br>');
-    print_r('Muchas gracias (PENDIENTE) !!');
+    print_r("<br>");
+    print_r("<h2>Muchas gracias (PENDIENTE) !!</h2>");
 
 ?>
