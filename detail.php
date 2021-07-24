91c339b6f2e5e6ca95ad7de5ad8b03a6517630b5
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-23 23:30:34 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -178,10 +178,10 @@
                                             </h3>
                                         </div>
                                         <h3 >
-                                            <?php echo $_POST['price'] ?>
+                                            <?php echo "$ " . $_POST['price'] ?>
                                         </h3>
                                         <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
+                                            <?php echo "Cantidad:" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
                                     <!--
