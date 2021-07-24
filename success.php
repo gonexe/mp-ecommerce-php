80dbf09f879bb5deed0b30c9f1c08fdbfb6c4112
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-24 02:24:35 -0300

    2

--- success.php
+++ success.php
@@ -3,7 +3,7 @@
     print_r('<h2>Visualización de resultado EXITOSO ($_GET):</h2>');
     print_r("<br>");
     foreach ($_GET as $key => $value) {
-        print_r("$key = $value");
+        print_r($key.' = "'.$value.'"');
         print_r("<br>");
     }
     print_r("<br>");
