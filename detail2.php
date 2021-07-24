3999d7e3407c6490159df95f89935db723562cce
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-23 22:06:55 -0300

    Update detail2.php

--- detail2.php
+++ detail2.php
@@ -3,7 +3,7 @@
     require __DIR__ .  '/vendor/autoload.php';
     
     // Agrega credenciales
-    MercadoPago\SDK::setAccessToken('TEST-7892157177460921-030322-ceb964be4439a87efac7b39c63718e09-31823309');
+    MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();
@@ -18,35 +18,35 @@
 
 ?>
 <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width-device-width, initial-scale=1.0">
-            <title>Prueba Gonzalo</title>
-        </head>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>Prueba Gonzalo</title>
+    </head>
 
-        <script src="https://sdk.mercadopago.com/js/v2"></script>
-        
-        <script>
-            const mp = new MercadoPago('TEST-df699538-52a2-4f37-8bae-f7df14723a8d', {
-                    locale: 'es-AR'
-            });
+    <script src="https://sdk.mercadopago.com/js/v2"></script>
+    
+    <script>
+        const mp = new MercadoPago('dev_24c65fb163bf11ea96500242ac130004', {
+            locale: 'es-AR'
+        });
 
-            // Inicializa el checkout
-            mp.checkout({
-                preference: {
-                    id: '<?php $preference->id; ?>'
-                },
-                render: {
-                        container: '.cho-container', // Indica el nombre de la clase donde se mostrará el botón de pago
-                        label: 'Pagar Gonzalo', // Cambia el texto del botón de pago (opcional)
-                }
-            });
-        </script>
+        // Inicializa el checkout
+        mp.checkout({
+            preference: {
+                id: '<?php $preference->id; ?>'
+            },
+            render: {
+                container: '.cho-container', // Indica el nombre de la clase donde se mostrará el botón de pago
+                label: 'Pagar Gonzalo', // Cambia el texto del botón de pago (opcional)
+            }
+        });
+    </script>
 
     <body>
         <form action="detail2.php" method="POST">
-            <div class=".cho-container">
+            <div class="cho-container">
 
             </div>
         </form>
