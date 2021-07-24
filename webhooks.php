0fd6b9f5e2b33c933db463bd1dff65a3909a6470
Gonzalo Avila <gonzalo.avila@trenesargentinos.gob.ar>
2021-07-24 03:11:53 -0300

    add

---
+++ webhooks.php
@@ -0,0 +1,14 @@
+<?php
+
+    print_r("GET");
+    print_r("<br>");
+    print_r($_GET);
+    print_r("<br>");
+    print_r("POST");
+    print_r("<br>");
+    print_r($_POST);
+    print_r("SERVER");
+    print_r("<br>");
+    print_r($_SERVER);
+
+?>
